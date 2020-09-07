158dbf1320456a93f73c137006eeccfb12684260
T7Q <tkuumola@gmail.com>
2020-09-07 21:12:24 +0300

    build: :construction: add data to pre-filled tables

---
+++ backend/config/data.sql
@@ -0,0 +1,57 @@
+INSERT INTO tags (tag_name)
+ VALUES ('Cooking'), ('Foodie'), ('Volunteering'), ('Trivia'), ('Movies'),
+ ('Art'), ('Outdoors'), ('Karaoke'), ('Cycling'), ('Spirituality'), ('Wine'),
+ ('Music'), ('Travel'), ('Gamer'), ('Vlogging'), ('Athlete'), ('Photography'),
+ ('Astrology'), ('DIY'), ('Surfing'), ('Museum'), ('Dog lover'), ('Comedy'),
+ ('Instagram'), ('Environmentalism'), ('Language Exchange'), ('Gardening'),
+ ('Disney'), ('Craft Beer'), ('Running'), ('Grab a drink'), ('Brunch'), ('Yoga'),
+ ('Fashion'), ('Dancing'), ('Working out'), ('Soccer'), ('Climbing'), ('Cat lover'),
+ ('Netflix'), ('Board Games'), ('Blogging'), ('Writer'), ('Reading'), ('Fishing'),
+ ('Swimming'), ('Shopping'), ('Politics'), ('Picnicking'), ('Golf'), ('Sports'),
+ ('Hiking'), ('Coffee'), ('Tea'), ('Baking'), ('Walking');
+
+INSERT INTO chinese_horo_compatibility(sign_1, sign_2, compatibility_value)
+ VALUES 
+ ('Rat', 'Rat', 9), ('Ox', 'Rat', 6), ('Ox', 'Ox', 8), ('Tiger', 'Rat', 4), ('Tiger', 'Ox', 4),
+ ('Tiger', 'Tiger', 5), ('Rabbit', 'Rat', 7), ('Rabbit', 'Ox', 8), ('Rabbit', 'Tiger', 5),
+ ('Rabbit', 'Rabbit', 8), ('Dragon', 'Rat', 10), ('Dragon', 'Ox', 7), ('Dragon', 'Tiger', 6),
+ ('Dragon', 'Rabbit', 7), ('Dragon', 'Dragon', 9), ('Snake', 'Rat', 7), ('Snake', 'Ox', 9),
+ ('Snake', 'Tiger', 3), ('Snake', 'Rabbit', 7), ('Snake', 'Dragon', 8), ('Snake', 'Snake', 8),
+ ('Horse', 'Rat', 3), ('Horse', 'Ox', 5), ('Horse', 'Tiger', 9), ('Horse', 'Rabbit', 5),
+ ('Horse', 'Dragon', 8), ('Horse', 'Snake', 4), ('Horse', 'Horse', 8), ('Goat', 'Rat', 4),
+ ('Goat', 'Ox', 2), ('Goat', 'Tiger', 4), ('Goat', 'Rabbit', 9), ('Goat', 'Dragon', 7),
+ ('Goat', 'Snake', 7), ('Goat', 'Horse', 8), ('Goat', 'Goat', 9), ('Monkey', 'Rat', 10),
+ ('Monkey', 'Ox', 4), ('Monkey', 'Tiger', 2), ('Monkey', 'Rabbit', 4), ('Monkey', 'Dragon', 10),
+ ('Monkey', 'Snake', 4), ('Monkey', 'Horse', 5), ('Monkey', 'Goat', 5), ('Monkey', 'Monkey', 9),
+ ('Rooster', 'Rat', 6), ('Rooster', 'Ox', 9), ('Rooster', 'Tiger', 8), ('Rooster', 'Rabbit', 2),
+ ('Rooster', 'Dragon', 9), ('Rooster', 'Snake', 9), ('Rooster', 'Horse', 6), ('Rooster', 'Goat', 5),
+ ('Rooster', 'Monkey', 4), ('Rooster', 'Rooster', 4), ('Dog', 'Rat', 8), ('Dog', 'Ox', 7),
+ ('Dog', 'Tiger', 9), ('Dog', 'Rabbit', 8), ('Dog', 'Dragon', 2), ('Dog', 'Snake', 8),
+ ('Dog', 'Horse', 9), ('Dog', 'Goat', 4), ('Dog', 'Monkey', 8), ('Dog', 'Rooster', 5),
+ ('Dog', 'Dog', 7), ('Pig', 'Rat', 8), ('Pig', 'Ox', 7), ('Pig', 'Tiger', 7), ('Pig', 'Rabbit', 9),
+ ('Pig', 'Dragon', 8), ('Pig', 'Snake', 4), ('Pig', 'Horse', 6), ('Pig', 'Goat', 9),
+ ('Pig', 'Monkey', 7), ('Pig', 'Rooster', 5), ('Pig', 'Dog', 7), ('Pig', 'Pig', 8);
+
+INSERT INTO western_horo_compatibility (sign_1, sign_2, compatibility_value)
+ VALUES
+ ('Aquarius', 'Aquarius', 9), ('Aries', 'Aquarius', 9), ('Aries', 'Aries', 10), ('Cancer', 'Aquarius', 4),
+ ('Cancer', 'Aries', 7), ('Cancer', 'Cancer', 10), ('Capricorn', 'Aquarius', 4), ('Capricorn', 'Aries', 6),
+ ('Capricorn', 'Cancer', 9), ('Capricorn', 'Capricorn', 9), ('Gemini', 'Aquarius', 10),
+ ('Gemini', 'Aries', 8), ('Gemini', 'Cancer', 5), ('Gemini', 'Capricorn', 5), ('Gemini', 'Gemini', 8),
+ ('Leo', 'Aquarius', 9), ('Leo', 'Aries', 9), ('Leo', 'Cancer', 5), ('Leo', 'Capricorn', 4),
+ ('Leo', 'Gemini', 8), ('Leo', 'Leo', 7), ('Libra', 'Aquarius', 9), ('Libra', 'Aries', 9),
+ ('Libra', 'Cancer', 5), ('Libra', 'Capricorn', 7), ('Libra', 'Gemini', 9), ('Libra', 'Leo', 8),
+ ('Libra', 'Libra', 9), ('Pisces', 'Aquarius', 7), ('Pisces', 'Aries', 5), ('Pisces', 'Cancer', 10),
+ ('Pisces', 'Capricorn', 9), ('Pisces', 'Gemini', 5), ('Pisces', 'Leo', 5), ('Pisces', 'Libra', 5),
+ ('Pisces', 'Pisces', 5), ('Sagittarius', 'Aquarius', 6), ('Sagittarius', 'Aries', 8),
+ ('Sagittarius', 'Cancer', 5), ('Sagittarius', 'Capricorn', 4), ('Sagittarius', 'Gemini', 8),
+ ('Sagittarius', 'Leo', 7), ('Sagittarius', 'Libra', 8), ('Sagittarius', 'Pisces', 6),
+ ('Sagittarius', 'Sagittarius', 8), ('Scorpio', 'Aquarius', 8), ('Scorpio', 'Aries', 6),
+ ('Scorpio', 'Cancer', 9), ('Scorpio', 'Capricorn', 9), ('Scorpio', 'Gemini', 4), ('Scorpio', 'Leo', 7),
+ ('Scorpio', 'Libra', 5), ('Scorpio', 'Pisces', 8), ('Scorpio', 'Sagittarius', 4), ('Scorpio', 'Scorpio', 10),
+ ('Taurus', 'Aquarius', 6), ('Taurus', 'Aries', 6), ('Taurus', 'Cancer', 9), ('Taurus', 'Capricorn', 10),
+ ('Taurus', 'Gemini', 5), ('Taurus', 'Leo', 6), ('Taurus', 'Libra', 6), ('Taurus', 'Pisces', 6),
+ ('Taurus', 'Sagittarius', 5), ('Taurus', 'Scorpio', 9), ('Taurus', 'Taurus', 9), ('Virgo', 'Aquarius', 7),
+ ('Virgo', 'Aries', 7), ('Virgo', 'Cancer', 8), ('Virgo', 'Capricorn', 9), ('Virgo', 'Gemini', 8),
+ ('Virgo', 'Leo', 5), ('Virgo', 'Libra', 6), ('Virgo', 'Pisces', 9), ('Virgo', 'Sagittarius', 7),
+ ('Virgo', 'Scorpio', 9), ('Virgo', 'Taurus', 9), ('Virgo', 'Virgo', 9);

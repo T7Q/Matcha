cda67112e8e0af425850fc8860fff6dcb379b759
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-09-08 16:12:30 +0300

    build: :construction: Update backend logic

---
+++ backend/controllers/account/pwdReset.js
@@ -0,0 +1,13 @@
+const accountModel = require('../../models/account');
+
+module.exports = async (req, res) => {
+    return res.send('reset a password');
+    // const { email, password } = req.body;
+    // if (!email || !password) {
+    //     return res.status(400).send({ 'msg': 'Invalid parameters' });
+    // }
+    // const user = await accountModel.findUser(email);
+    // if (user)
+    //     return res.status(400).send({ 'msg': 'User already exists' });
+    // return accountModel.register(req, res);
+}

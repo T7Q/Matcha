4ba05cfed940601a6ef524cd2e0c84efc43b362c
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-09-24 00:32:23 +0300

    feat: :sparkles: add auth path to account

---
+++ backend/controllers/account/auth.js
@@ -0,0 +1,14 @@
+const accountModel = require('../../models/account');
+
+module.exports = async (req, res) => {
+    const userId = req.user.userId;
+
+    try {
+        const result = await accountModel.findUserInfo('user_id', userId, 'status', 'username', 'email');
+        result.userId = userId;
+        return res.json(result);
+    } catch (e) {
+        console.log(e);
+        return res.json({ error: 'something went wrong' });
+    }
+};

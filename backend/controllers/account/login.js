0bf521c8dc8c411ae763ecaefbcea2b83d177b28
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-09-23 12:32:03 +0300

    build: :construction: implement front end register

--- backend/controllers/account/login.js
+++ backend/controllers/account/login.js
@@ -8,7 +8,7 @@ module.exports = async (req, res) => {
     let { username, password } = req.body;
 
     if (!username || !password) {
-        return res.status(400).json({ 'error': 'Fields can not be empty' });
+        return res.status(400).json({ error: 'Fields can not be empty' });
     }
 
     username = username.toLowerCase();
@@ -17,12 +17,12 @@ module.exports = async (req, res) => {
 
         // check if account exists and password correct
         if (!user || Object.keys(user).length == 0 || !(await bcrypt.compare(password, user.password))) {
-            return res.status(401).json({ 'error': 'Invalid credentials' });
+            return res.status(401).json({ error: 'Invalid credentials' });
         }
 
         // check if account activated
         if (user.status === 0) {
-            return res.json({ 'error': 'Your account is not activated yet. Please, check your email.' });
+            return res.status(401).json({ error: 'Your account is not activated yet. Please, check your email.' });
         }
 
         // get location
@@ -31,15 +31,19 @@ module.exports = async (req, res) => {
         data.online = 1;
         await accountModel.updateAccount(user.user_id, data);
         return res.json({
-            'status': user.status,
-            'username': username,
-            'tkn': jwt.sign({
-                userId: user.user_id,
-                status: user.status
-            }, jwtSecret, { expiresIn: 1000 * 60 * 60 })
+            status: user.status,
+            username: username,
+            tkn: jwt.sign(
+                {
+                    userId: user.user_id,
+                    status: user.status,
+                },
+                jwtSecret,
+                { expiresIn: 1000 * 60 * 60 }
+            ),
         });
     } catch (e) {
         console.log(e);
         return res.status(400).json();
     }
-}
+};
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,47 @@
-import React, { Fragment } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import Likes from "./components/likes/Likes";
-import Matches from "./components/matches/Matches";
-import Profile from "./components/profile/Profile";
-import CreateProfile from "./components/profile-form/CreateProfile";
-import Chat from "./components/chat/Chat";
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Likes from './components/likes/Likes';
+import Matches from './components/matches/Matches';
+import Profile from './components/profile/Profile';
+import CreateProfile from './components/profile-form/CreateProfile';
+import Chat from './components/chat/Chat';
+import Alert from './components/layout/Alert';
+import setAuthToken from './utils/setAuthToken';
 
-// Redux 
-import { Provider } from "react-redux";
+// Redux
+import { Provider } from 'react-redux';
 import store from './store';
 
-import "./App.css";
+import './App.css';
+
+if (localStorage.getItem('token')) {
+    setAuthToken(localStorage.getItem('token'));
+}
 
 const App = () => (
-  <Provider store={store}>
-    <Router>
-        <Fragment>
-            <Navbar />
-            <Route exact path="/" component={Landing} />
-            <section className="container">
-                <Switch>
-                    <Route exact path="/login" component={Login}/>
-                    <Route exact path="/register" component={Register}/>
-                    <Route exact path="/matches" component={Matches}/>
-                    <Route exact path="/messages" component={Chat}/>
-                    <Route exact path="/likes" component={Likes}/>
-                    <Route exact path="/profile" component={Profile}/>
-                    <Route exact path="/create-profile" component={CreateProfile}/>
-                </Switch>
-            </section>
-        </Fragment>
-    </Router>
-  </Provider>
+    <Provider store={store}>
+        <Router>
+            <Fragment>
+                <Navbar />
+                <Route exact path='/' component={Landing} />
+                <section className='container'>
+                    <Switch>
+                        <Route exact path='/login' component={Login} />
+                        <Route exact path='/register' component={Register} />
+                        <Route exact path='/matches' component={Matches} />
+                        <Route exact path='/messages' component={Chat} />
+                        <Route exact path='/likes' component={Likes} />
+                        <Route exact path='/profile' component={Profile} />
+                        <Route exact path='/create-profile' component={CreateProfile} />
+                    </Switch>
+                </section>
+            </Fragment>
+        </Router>
+    </Provider>
 );
 
 export default App;

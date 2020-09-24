af0cd3a8e310a14f4b55e3a102ad974c239d9a0a
T7Q <tkuumola@gmail.com>
2020-09-24 12:48:26 +0300

    feat: :zap: add getUserTags for profile

--- backend/controllers/profile/show.js
+++ backend/controllers/profile/show.js
@@ -9,6 +9,13 @@ const userProfile = async (req, res) => {
         let userInfo = await accountModel.findUserInfo("user_id", req.body.user_id, "user_id", "fame_rating",
         "bio", "first_name", "last_name", 
         "chinese_horo", "western_horo", "gender", "sex_preference", "birth_date");
+        let tags = await profileModel.getUserTags(req.body.user_id);
+        if(tags.rowCount > 0){
+            userInfo['tags'] = [];
+            tags['rows'].forEach(value =>{
+                userInfo['tags'].push(value.tag_name)
+            })
+        }
         return res.json(userInfo);
     } catch (e) {
         return res.json({ error: "Error getting profile info" });
@@ -16,12 +23,19 @@ const userProfile = async (req, res) => {
 };
 
 const getProfile = async (req, res) => {
-    let user_id = 1;    
-
+    let user_id = 1;    /// ADD FROM AUTH 
+ 
     try {
         let userInfo = await accountModel.findUserInfo("user_id", user_id, "user_id", "fame_rating",
         "bio", "first_name", "last_name", 
         "chinese_horo", "western_horo", "gender", "sex_preference", "birth_date");
+        let tags = await profileModel.getUserTags(user_id);
+        if(tags.rowCount > 0){
+            userInfo['tags'] = [];
+            tags['rows'].forEach(value =>{
+                userInfo['tags'].push(value.tag_name)
+            })
+        }
         return res.json(userInfo);
     } catch (e) {
         return res.json({ error: "Error getting profile info" });

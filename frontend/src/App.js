deb09b163c12af893b4a98aa51da25462aee442d
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-09-29 18:44:35 +0300

    styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
-import { Container } from '@material-ui/core';
+import { Container, Grid, Box } from '@material-ui/core';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
@@ -25,7 +25,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 import theme from './styles/custom';
 import CssBaseline from '@material-ui/core/CssBaseline';
-// import './App.css';
+import './App.css';
 
 // setAuthToken(localStorage.getItem('token'));
 
@@ -39,26 +39,39 @@ const App = () => {
             <Provider store={store}>
                 <Router>
                     <CssBaseline />
-                    <Container maxWidth='sm'>
-                        <Navbar />
-                        <Route exact path='/' component={Landing} />
-                        <section className='container'>
-                            <Alert />
-                            <Switch>
-                                <Route exact path='/login' component={Login} />
-                                <Route exact path='/register' component={Register} />
-                                <Route exact path='/forgetPwd' component={ForgotPwd} />
-                                <Route exact path='/updatePwd' component={UpdatePwd} />
-                                <Route exact path='/message' component={Message} />
-                                <PrivateRoute exact path='/messages' component={Chat} />
-                                <PrivateRoute exact path='/likes' component={Matches} />
-                                <PrivateRoute exact path='/profile' component={Profile} />
-                                <Route exact path='/create-profile' component={CreateProfile} />
-                                <Route exact path='/complete' component={ProfileCreation} />
-                                <PrivateRoute exact path='/matches' component={Matches} />
-                            </Switch>
-                        </section>
-                        <Footer />
+                    <Container maxWidth='md'>
+                        <Box display='flex' flexDirection='column' minHeight='100vh'>
+                            <Box>
+                                <Navbar />
+                            </Box>
+                            <Box flexGrow={1}>
+                                <Grid container spacing={1}>
+                                    <Grid container item xs={12} sm={6}>
+                                        <Route exact path='/' component={Landing} />
+                                        <section className='container'>
+                                            <Alert />
+                                            <Switch>
+                                                <Route exact path='/login' component={Login} />
+                                                <Route exact path='/register' component={Register} />
+                                                <Route exact path='/forgetPwd' component={ForgotPwd} />
+                                                <Route exact path='/updatePwd' component={UpdatePwd} />
+                                                <Route exact path='/message' component={Message} />
+                                                <PrivateRoute exact path='/messages' component={Chat} />
+                                                <PrivateRoute exact path='/likes' component={Matches} />
+                                                <PrivateRoute exact path='/profile' component={Profile} />
+                                                <Route exact path='/create-profile' component={CreateProfile} />
+                                                <Route exact path='/complete' component={ProfileCreation} />
+                                                <PrivateRoute exact path='/matches' component={Matches} />
+                                            </Switch>
+                                        </section>
+                                    </Grid>
+                                    <Grid container item xs={12} sm={6}></Grid>
+                                </Grid>
+                            </Box>
+                            <Box>
+                                <Footer />
+                            </Box>
+                        </Box>
                     </Container>
                 </Router>
             </Provider>

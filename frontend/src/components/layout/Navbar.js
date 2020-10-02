02af3d2a73a52b3fc4d4da62c7cc37b9ea9de2a7
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-10-02 14:59:35 +0300

    make responsive navbar

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 import { logout } from '../../actions/auth';
 import {
     MessageOutlined,
@@ -48,6 +48,40 @@ const StyledMenu = withStyles(theme => ({
         {...props}
     />
 ));
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexFlow: 1,
+    },
+    text: {
+        padding: theme.spacing(2, 2, 0),
+    },
+    paper: {
+        paddingBottom: 50,
+    },
+    list: {
+        marginBottom: theme.spacing(2),
+    },
+    subheader: {
+        backgroundColor: theme.palette.background.paper,
+    },
+    appBar: {
+        top: 'auto',
+        bottom: 0,
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    fabButton: {
+        position: 'absolute',
+        zIndex: 1,
+        top: -30,
+        left: 0,
+        right: 0,
+        margin: '0 auto',
+    },
+}));
+
 const StyledMenuItem = withStyles(theme => ({
     root: {
         color: '#fff',
@@ -63,117 +97,105 @@ const StyledMenuItem = withStyles(theme => ({
     },
 }))(MenuItem);
 
-const Navbar = ({ logout, auth: { isAuthenticated, loading, user } }) => {
+const Navbar = ({ logout, auth: { isAuthenticated, loading, user }, history }) => {
     const [profileSettings, setProfileSettings] = useState(null);
+    const classes = useStyles();
 
-    const handleClick = event => {
+    const handleClick = (event, newRoute) => {
         setProfileSettings(event.currentTarget);
+    };
+
+    const handleProfile = newRoute => {
+        history.push(newRoute);
     };
 
     const handleClose = () => {
         setProfileSettings(null);
     };
 
-    const userStatus1 = (
-        <Link className="link-decor" to="/" onClick={logout}>
-            <Typography variant="h6" color="textPrimary">
-                <i className="fas fa-sign-out-alt"></i> Logout
-            </Typography>
-        </Link>
-    );
-
-    const authLinks = [
-        <>
-            <Link className="link-decor" to="/matches">
-                <Typography className="pr-5" variant="h6" color="textPrimary">
-                    <Badge className="pr-2">
-                        <PeopleOutline />
-                    </Badge>
-                    Matches
-                </Typography>
-            </Link>
-            <Link className="link-decor" to="/messages">
-                <Typography className="pr-5" variant="h6" color="textPrimary">
-                    <Badge className="pr-2">
-                        <MessageOutlined />
-                    </Badge>
-                    Messages
-                </Typography>
-            </Link>
-            <Link className="link-decor" to="/likes">
-                <Typography className="pr-5" variant="h6" color="textPrimary">
-                    <Badge className="pr-2">
-                        <FavoriteBorder />
-                    </Badge>
-                    Likes
-                </Typography>
-            </Link>
-        </>,
-        <>
-            <Link className="link-decor" to="">
-                <Typography variant="h6" className="pr-5" color="textSecondary">
-                    <Button onClick={handleClick}>
-                        <Badge className="pr-2">
-                            <AccountCircle />
-                        </Badge>
-                        Profile
-                    </Button>
-                    <StyledMenu
-                        getContentAnchorEl={null}
-                        anchorOrigin={{
-                            vertical: 'bottom',
-                            horizontal: 'center',
-                        }}
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'center',
-                        }}
-                        anchorEl={profileSettings}
-                        keepMounted
-                        open={Boolean(profileSettings)}
-                        onClose={handleClose}>
-                        <StyledMenuItem onClick={handleClose}>
-                            <Link className="link-decor" to="/profile">
-                                <ListItemIcon>My profile</ListItemIcon>
-                            </Link>
-                        </StyledMenuItem>
-                        <StyledMenuItem onClick={handleClose}>
-                            <Link className="link-decor" to="/profile">
-                                <ListItemIcon>Account Settings</ListItemIcon>
-                            </Link>
-                        </StyledMenuItem>
-                        <StyledMenuItem onClick={handleClose}>
-                            <Link className="link-decor" to="/profile">
-                                <ListItemIcon>Visit history</ListItemIcon>
-                            </Link>
-                        </StyledMenuItem>
-                    </StyledMenu>
-                </Typography>
-            </Link>
-            <Link className="link-decor" to="/" onClick={logout}>
-                <Typography variant="h6" color="textPrimary">
-                    <i className="fas fa-sign-out-alt"></i> Logout
-                </Typography>
-            </Link>
-        </>,
-    ];
-
     return (
-        <Box maxWidth="100%">
-            <AppBar color="secondary" position="static">
-                <Toolbar className="flex">
+        <div className={classes.root}>
+            <AppBar color="secondary">
+                <Toolbar>
                     <Box display="flex" flexGrow={1}>
                         <Link className="link-decor" to="/">
                             <Typography className="pr-5" color="textSecondary" variant="h6">
                                 <i className="fas fa-moon" /> Astro Matcha
                             </Typography>
                         </Link>
-                        {!loading && isAuthenticated && user.status !== 1 && authLinks[0]}
+                        <Link className="link-decor" to="/matches">
+                            <Typography className="pr-5" variant="h6" color="textPrimary">
+                                <Badge className="pr-2">
+                                    <PeopleOutline />
+                                </Badge>
+                                Matches
+                            </Typography>
+                        </Link>
+                        <Link className="link-decor" to="/messages">
+                            <Typography className="pr-5" variant="h6" color="textPrimary">
+                                <Badge className="pr-2">
+                                    <MessageOutlined />
+                                </Badge>
+                                Messages
+                            </Typography>
+                        </Link>
+                        <Link className="link-decor" to="/likes">
+                            <Typography className="pr-5" variant="h6" color="textPrimary">
+                                <Badge className="pr-2">
+                                    <FavoriteBorder />
+                                </Badge>
+                                Likes
+                            </Typography>
+                        </Link>
                     </Box>
-                    {!loading && isAuthenticated ? (user.status === 1 ? userStatus1 : authLinks[1]) : null}
+                    <Box display="flex">
+                        <Typography variant="h6" className="pr-5" color="textSecondary">
+                            <Button onClick={handleClick}>
+                                <Badge className="pr-2">
+                                    <AccountCircle />
+                                </Badge>
+                                Profile
+                            </Button>
+                            <StyledMenu
+                                getContentAnchorEl={null}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'center',
+                                }}
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'center',
+                                }}
+                                anchorEl={profileSettings}
+                                keepMounted
+                                open={Boolean(profileSettings)}
+                                onClose={handleClose}>
+                                <StyledMenuItem onClick={() => handleProfile('/profile')}>
+                                    <Link className="link-decor" to="/profile">
+                                        <ListItemIcon>My profile</ListItemIcon>
+                                    </Link>
+                                </StyledMenuItem>
+                                <StyledMenuItem onClick={() => handleProfile('/profile')}>
+                                    <Link className="link-decor" to="/profile">
+                                        <ListItemIcon>Account Settings</ListItemIcon>
+                                    </Link>
+                                </StyledMenuItem>
+                                <StyledMenuItem onClick={() => handleProfile('/profile')}>
+                                    <Link className="link-decor" to="/profile">
+                                        <ListItemIcon>Visit history</ListItemIcon>
+                                    </Link>
+                                </StyledMenuItem>
+                            </StyledMenu>
+                        </Typography>
+                        <Link className="link-decor" to="/" onClick={logout}>
+                            <Typography variant="h6" color="textPrimary">
+                                <i className="fas fa-sign-out-alt"></i> Logout
+                            </Typography>
+                        </Link>
+                    </Box>
                 </Toolbar>
             </AppBar>
-        </Box>
+        </div>
     );
 };
 
@@ -186,4 +208,4 @@ const mapStateToProps = state => ({
     auth: state.auth,
 });
 
-export default connect(mapStateToProps, { logout })(Navbar);
+export default connect(mapStateToProps, { logout })(withRouter(Navbar));

72978b12c6816033b9e09c41faa942d9bc41c9ae
T7Q <tkuumola@gmail.com>
2020-10-06 23:14:56 +0300

    feat: :sparkles: continue on filters

--- frontend/src/components/matches/Filter.js
+++ frontend/src/components/matches/Filter.js
@@ -1,29 +1,83 @@
-import React, { Fragment, useEffect } from "react";
-import Switches from "./Switch";
-import RangeSlider from "./Slider";
-import MultipleSelect from "../matchGallery/MultipleSelect";
-import Temp from "./Temp";
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { updateFilter, resetFilter } from "../../actions/match";
 
+import MultipleSelect from "../common/matchGallery/MultipleSelect";
+import Switches from "../common/matchGallery/Switch";
+import RangeSlider from "../common/matchGallery/Slider";
+import Switch from '@material-ui/core/Switch';
 
-const Filter = () => {
+import Button from '@material-ui/core/Button';
+import Match from "../common/matchGallery/GetMatches";
+
+const Filter = ({updateFilter, resetFilter, match: { filter }}) => {
+    const [filterIsOn, setFilter] = React.useState(0);
     const orientation = ["gay", "lesbian", "straight woman", "straight man", "bi"];
     const tags = ['Cooking', 'Art', 'Games'];
-    // console.log("filter initiated");
+    console.log("FILTER component");
+
     const countries = ["Finland", "Estonia", "Brazil"];
+    useEffect(() => {
+        updateFilter(filter);
+    }, [updateFilter, filter]);
+
+    const handleChange = (event) => {
+        console.log("HANDLE CHANGE");
+        updateFilter({...filter, [event.target.name]: event.target.checked});
+    };
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        console.log("HANDLE cLICK");
+        // updateFilter({...filter});
+        setFilter(filterIsOn + 1);
+    };
+
+    const handleClickReset = (e) => {
+        e.preventDefault();
+        console.log("HANDLE cLICK");
+        resetFilter();
+    };
+    
     return (
-        <div>
-            <Temp>Temp</Temp>
-            <Switches title="Chinese horoscope"></Switches>
-            {/* <CustomizedSwitches title="Chinese horoscope">Here</CustomizedSwitches>
-            <CustomizedSwitches title="Western horoscope">Here</CustomizedSwitches> */}
+        <>
+            <Switch
+                checked={filter.believe_cn}
+                onChange={handleChange}
+                color="primary"
+                name="believe_cn"
+                inputProps={{ 'aria-label': 'secondary checkbox' }}
+            />
+            <Switch
+                checked={filter.believe_west}
+                onChange={handleChange}
+                color="primary"
+                name="believe_west"
+                inputProps={{ 'aria-label': 'secondary checkbox' }}
+            />
+            
             <RangeSlider title="Distance 0 - 20,020 km" min={0} max={20020}>SLider</RangeSlider>
             <RangeSlider title="Age 18 - 120" min={18} max={120}>SLider</RangeSlider>
             <RangeSlider title="Fame rating 0 - 5" min={0} max={5}>SLider</RangeSlider>
             {/* <MultipleSelect names={countries}>Country</MultipleSelect> */}
-            <MultipleSelect names={tags} title="Interests">Interests</MultipleSelect>
+            {/* <MultipleSelect names={tags} title="Interests">Interests</MultipleSelect> */}
             {/* <MultipleSelect names={orientation}>Sex orintation</MultipleSelect> */}
-        </div>
+            <Button variant="contained" color="primary" onClick={handleClick}>See results</Button>
+            <Button variant="contained" color="primary" onClick={handleClickReset}>Reset</Button>
+            <Match route="/match/search" filterIsOn={filterIsOn}/>
+        </>
     );
 }
 
-export default Filter;
+Filter.propTypes = {
+    updateFilter: PropTypes.func.isRequired,
+    resetFilter: PropTypes.func.isRequired,
+    match: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+    match: state.match,
+});
+
+export default connect(mapStateToProps, { updateFilter, resetFilter })(Filter);

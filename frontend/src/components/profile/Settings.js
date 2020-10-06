a1332a3ff463209c33e2330a9c52bf66d25c0dd0
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-10-06 23:18:16 +0300

    work on settings continue

--- frontend/src/components/profile/Settings.js
+++ frontend/src/components/profile/Settings.js
@@ -3,18 +3,21 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Redirect, withRouter } from 'react-router-dom';
 import { Box, Typography, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
-import { setAlert } from '../../actions/alert';
 import { register } from '../../actions/auth';
 import Input from '../common/Input';
-import WizardForm from '../common/WizardForm';
 import InboxIcon from '@material-ui/icons/Inbox';
 import DraftsIcon from '@material-ui/icons/Drafts';
 
 const Settings = () => {
     return (
         <Box>
-            <Box textAlign="center">
-                <Typography>Account Settings</Typography>
+            <Box
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                bgcolor="primary.main"
+                height="80px">
+                <Typography variant="h5">Account Settings</Typography>
             </Box>
             <Box display="flex">
                 <Box>

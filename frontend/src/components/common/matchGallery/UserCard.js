6629de8432a5fb5ec751ffc3b1813d480356880e
T7Q <tkuumola@gmail.com>
2020-10-08 23:12:43 +0300

    feat: :sparkles: render image on front end from server

--- frontend/src/components/common/matchGallery/UserCard.js
+++ frontend/src/components/common/matchGallery/UserCard.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-// import CardMedia from "@material-ui/core/CardMedia";
+import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
@@ -16,11 +16,6 @@ const useStyles = makeStyles({
     root: {
         minWidth: 200,
     },
-    bullet: {
-        display: "inline-block",
-        margin: "0 2px",
-        transform: "scale(0.8)",
-    },
     title: {
         fontSize: 14,
     },
@@ -32,47 +27,43 @@ const useStyles = makeStyles({
 const UserCard = ({ card }) => {
     const classes = useStyles();
     card.fame = parseFloat(card.fame);
-
+    // console.log("card", card);
     return (
-            <Grid item xs={12} sm={4}>
-                {/* <img src={str}></img> */}
-                <Card className={classes.root}>
-                    {/* <CardMedia
-                        className={classes.media}
-                        src={require(`"/images/${card.profile_pic_path}"`)}
-                        title="Profile_pic"
-                    /> */}
-                    <CardContent>
-                        <Typography
-                            className={classes.title}
+        <Grid item xs={12} sm={4}>
+            <Card className={classes.root}>
+                <CardMedia
+                    component="img"
+                    alt="Contemplative Reptile"
+                    //   height="140"
+                    image={"/" + card.profile_pic_path}
+                    // image="/Photo_1601037282389_683.png"
+                    title="Contemplative Reptile"
+                />
 
-                            gutterBottom
-                        >
-                            {card.user_id}
-                        </Typography>
-                        <Typography variant="h5" component="h2">
-                            {card.first_name}, {card.age}
-                        </Typography>
-                        <Typography
-                            className={classes.pos}
-
-                        >
-                            {card.distance === null ? 0 : card.distance} km
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <StarBorderIcon label="10">10</StarBorderIcon>
-                        <Button size="small">{card.match.toFixed(0)}%</Button>
-                        <Button size="small">{card.fame === null ? 0 : card.fame.toFixed(1)}</Button>
-                        {/* <Connection connected={card.connected} /> */}
-                        <IconButton aria-label="chat">
-                            {card.connected ?
-                            <ChatIcon />:
-                                <FavoriteIcon />}
-                            </IconButton>
-                    </CardActions>
-                </Card>
-     </Grid>
+                <CardContent>
+                    <Typography className={classes.title} gutterBottom>
+                        {card.user_id}
+                    </Typography>
+                    <Typography variant="h5" component="h2">
+                        {card.first_name}, {card.age}
+                    </Typography>
+                    <Typography className={classes.pos}>
+                        {card.distance === null ? 0 : card.distance} km
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <StarBorderIcon label="10">10</StarBorderIcon>
+                    <Button size="small">{card.match.toFixed(0)}%</Button>
+                    <Button size="small">
+                        {card.fame === null ? 0 : card.fame.toFixed(1)}
+                    </Button>
+                    {/* <Connection connected={card.connected} /> */}
+                    <IconButton aria-label="chat">
+                        {card.connected ? <ChatIcon /> : <FavoriteIcon />}
+                    </IconButton>
+                </CardActions>
+            </Card>
+        </Grid>
     );
 };
 

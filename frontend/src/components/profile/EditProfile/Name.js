fa254409d54aff98778bf9f258eb69af9ebb4cac
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-10-13 12:24:28 +0300

    refactor: :construction: work on profile editing

--- frontend/src/components/profile/EditProfile/Name.js
+++ frontend/src/components/profile/EditProfile/Name.js
@@ -2,21 +2,35 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { TextField, FormGroup, Grid, Button } from '@material-ui/core';
 import { useStyles } from '../../../styles/custom';
+import WizardForm from '../../common/WizardForm';
+import Input from '../../common/Input';
 
-const Name = (firstName, lastName) => {
+const Name = ({ firstName, lastName }) => {
     const [formData, setFormData] = useState({
         firstname: firstName,
         lastname: lastName,
     });
 
+    const [errors, setErrors] = useState({
+        firstnameError: '',
+        lastnameError: '',
+    });
     const { firstname, lastname } = formData;
+    const { firstnameError, lastnameError } = errors;
     const classes = useStyles();
 
-    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+    const onChange = e => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+        validateLocal(e.target.name, e.target.value);
+    };
 
     const handleSubmit = async event => {
-        event.preventDefault();
         try {
+            console.log(lastname, firstname);
+            const isValid = validate();
+            if (isValid) {
+                console.log('no errors');
+            }
             // const res = await axios.post('/profile/edit', { key: 'name', value: formData });
             // console.log('edit profile actions', res.data);
         } catch (error) {
@@ -24,43 +38,73 @@ const Name = (firstName, lastName) => {
         }
     };
 
+    const validateLocal = (type, value) => {
+        const errorType = [type] + 'Error';
+        if (!value) {
+            setErrors({ ...errors, [errorType]: 'required field' });
+        } else {
+            const re = /^[A-Za-z0-9]{0,}$/;
+            if (!re.test(value)) {
+                setErrors({
+                    ...errors,
+                    [errorType]: 'Name must include letters and numbers only',
+                });
+            } else {
+                setErrors({ ...errors, [errorType]: '' });
+            }
+        }
+    };
+
+    const validate = () => {
+        let errorName = '';
+        console.log('in validate ', 'first: ', firstname, 'last: ', lastname);
+        if (!firstname) {
+            errorName = 'required field';
+        } else {
+            const re = /^[A-Za-z0-9]{0,}$/;
+            if (!re.test(firstname)) {
+                errorName = 'Name must include letters and numbers only';
+            }
+        }
+        if (!lastname) {
+            setErrors({ ...errors, firstnameError: errorName, lastnameError: 'required field' });
+        } else {
+            const re = /^[A-Za-z0-9]{0,}$/;
+            if (!re.test(lastname)) {
+                setErrors({
+                    ...errors,
+                    firstnameError: errorName,
+                    lastnameError: 'Name must include letters and numbers only',
+                });
+            } else if (errorName) {
+                setErrors({ ...errors, firstnameError: errorName });
+            } else {
+                setErrors({ ...errors, firstnameError: '', lastnameError: '' });
+                return true;
+            }
+        }
+        return false;
+    };
+
     return (
-        <form onSubmit={handleSubmit}>
-            <FormGroup>
-                <Grid container direction="column" spacing={1}>
-                    <Grid item>
-                        <TextField
-                            variant="outlined"
-                            name="firstname"
-                            type="text"
-                            className={classes.customInput}
-                            placeholder="new email"
-                            value={email}
-                            onChange={onChange}
-                        />
-                    </Grid>
-                    <Grid item>
-                        <TextField
-                            className={classes.customInput}
-                            variant="outlined"
-                            type="password"
-                            name="password"
-                            placeholder="password"
-                            value={password}
-                            onChange={onChange}
-                        />
-                    </Grid>
-                </Grid>
-                <Button
-                    type="submit"
-                    size="small"
-                    variant="contained"
-                    color="primary"
-                    className={`${classes.customButton} ${classes.p2}`}>
-                    Save
-                </Button>
-            </FormGroup>
-        </form>
+        <WizardForm header="Edit name" formData={formData} setFormData={setFormData} onSubmit={handleSubmit}>
+            <>
+                <Input
+                    name="firstname"
+                    type="firstname"
+                    value={firstname}
+                    handleChange={onChange}
+                    helperText={firstnameError}
+                />
+                <Input
+                    name="lastname"
+                    type="lastname"
+                    value={lastname}
+                    handleChange={onChange}
+                    helperText={lastnameError}
+                />
+            </>
+        </WizardForm>
     );
 };
 
--- frontend/src/components/profile/EditProfile/index.js
+++ frontend/src/components/profile/EditProfile/index.js
@@ -1,31 +1,92 @@
-import React, { Fragment, useState } from 'react';
-import { withRouter } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Box, Typography, Grid, Tab, Tabs, useMediaQuery } from '@material-ui/core';
+import { useTheme } from '@material-ui/core/styles';
+import { NotificationsActive, Lock, Email, Block, Delete } from '@material-ui/icons';
+import { getProfile } from '../../../actions/profile';
+import Spinner from '../../layout/Spinner';
+import Bio from './Bio';
+import Birthdate from './Birthdate';
+import Name from './Name';
+import Passion from './Passion';
+import Username from './Username';
+import SexPreference from './SexPreference';
+import Photos from './Photos';
 
-import { createProfile } from '../../../actions/profile';
+const Edit = ({ getProfile, profile: { profile, loading } }) => {
+    const [tab, setTab] = useState(0);
 
-const EditProfile = ({ history }) => {
-    const [formData, setFormData] = useState({
-        gender: '',
-        sex_preference: '',
-        bio: '',
-        birth_date: '',
-        tags: '',
-        country: '',
-    });
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('xs'));
 
-    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
-    const onSubmit = e => {
-        e.preventDefault();
-        createProfile(formData, history);
+    const handleChange = (event, newTab) => {
+        setTab(newTab);
     };
 
-    return <Fragment></Fragment>;
+    useEffect(() => {
+        getProfile('myProfile');
+    }, [getProfile]);
+
+    console.log(profile);
+
+    return loading ? (
+        <Spinner />
+    ) : (
+        <Box minHeight="80vh" display="flex" flexDirection="column">
+            <Box
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                bgcolor="secondary.main"
+                pl={8}
+                height="80px">
+                <Typography variant="h5">Profile edit</Typography>
+            </Box>
+            <Box alignItems="center" flexGrow={1} display="flex">
+                <Grid container>
+                    <Grid container item sm={3} xs={12} justify="center">
+                        <Tabs
+                            indicatorColor="primary"
+                            textColor="primary"
+                            orientation="vertical"
+                            value={tab}
+                            onChange={handleChange}>
+                            <Tab style={{ display: 'none' }} label="hidden" />
+                            <Tab label="&emsp;Bio &emsp;&emsp;&emsp;&#8811;" icon={<NotificationsActive />} />
+                            <Tab label="&emsp;Birthdate&emsp;&#8811;" icon={<Lock />} />
+                            <Tab label="&emsp;Username &emsp;&emsp;&emsp;&#8811;" icon={<Email />} />
+                            <Tab label="&emsp;Passion &nbsp; &emsp;&emsp;&#8811;" icon={<Block />} />
+                            <Tab label="&emsp;Name &nbsp; &nbsp;&emsp;&#8811;" icon={<Delete />} />
+                            <Tab label="&emsp;Sex preference &nbsp; &nbsp;&emsp;&#8811;" icon={<Delete />} />
+                            <Tab label="&emsp;Photos &nbsp; &nbsp;&emsp;&#8811;" icon={<Delete />} />
+                        </Tabs>
+                    </Grid>
+                    <Grid container justify={isMobile ? 'center' : 'flex-start'} item sm={9} xs={12}>
+                        <Box pt={3} m={3}>
+                            {tab === 0 && <></>}
+                            {tab === 1 && <Bio bioProp={profile.bio} />}
+                            {tab === 2 && <Birthdate />}
+                            {tab === 3 && <Username usernameProp={profile.username} />}
+                            {tab === 4 && <Passion />}
+                            {tab === 5 && <Name firstName={profile.first_name} lastName={profile.last_name} />}
+                            {tab === 6 && <SexPreference />}
+                            {tab === 7 && <Photos />}
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Box>
+        </Box>
+    );
 };
 
-EditProfile.propTypes = {
-    // createProfile: PropTypes.func.isRequired,
+Edit.propTypes = {
+    getProfile: PropTypes.func.isRequired,
+    profile: PropTypes.object.isRequired,
 };
 
-export default connect(null, {})(withRouter(EditProfile));
+const mapStateToProps = state => ({
+    profile: state.profile,
+});
+
+export default connect(mapStateToProps, { getProfile })(Edit);

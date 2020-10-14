bd05877efa3aa00fa4e9d16e4fc19539da20984b
T7Q <tkuumola@gmail.com>
2020-10-14 14:01:50 +0300

    feat: :zap: fake users with debugging

--- backend/config/fakeusers.js
+++
@@ -1,5 +0,0 @@
-var faker = require('faker');
- 
-    var randomName = faker.name.findName(); // Rowan Nikolaus
-    var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
-    var randomCard = faker.helpers.createCard(); // random contact card containing many properties
--- backend/models/profile.js
+++ backend/models/profile.js
@@ -236,10 +236,34 @@ const getBlockedUsers = async (authUserId) => {
     return res.rows;
 };
 
-const insertFakeUsers = async () => {
-    var faker = require("faker");
-    console.log("got to insertfake");
-    const createFakeUser = () => {
+// ********* 
+const faker = require("faker");
+
+// number of generated fake accounts
+const desiredFakeUsers = 2;
+
+// statement to insert 21 params to table 'users'
+const prepareStmt = (desiredFakeUsers) => {
+    let str = "";
+    let params = 22;
+    let j = 1;
+    for (let k = 0; k < desiredFakeUsers; k++) {
+        str = str.concat("(");
+        for (let i = 1; i < params; i++) {
+            str = i === params - 1 ? str.concat(`$${j}`) : str.concat(`$${j},`);
+            j++;
+        }
+        str = k === desiredFakeUsers - 1 ? str.concat(")") : str.concat("),");
+    }
+    return `INSERT INTO users (username, first_name, last_name, email, status, 
+        password, birth_date, gender, sex_preference, email_notification,
+        online, latitude, longitude, country, real_time_notification,
+        fame_rating, bio, created_at, last_seen, profile_pic_path, fame_14_days)
+        VALUES ${str}`;
+};
+
+// generate 21 column values for each user with Faker.js
+const createFakeUser = () => {
     return [
         faker.internet.userName(),
         faker.name.firstName(),
@@ -261,42 +285,96 @@ const insertFakeUsers = async () => {
         faker.date.between("2018-01-01", Date()),
         faker.date.between("2018-01-01", Date()),
         faker.image.avatar(),
-        faker.random.number(30)
-    ]}
+        faker.random.number(30),
+    ];
+};
 
+// create an array with info of all users
+const generateFakeUsers = (desiredFakeUsers) => {
     let fakeUsers = [];
-    const desiredFakeUsers = 100;
     for (let i = 0; i < desiredFakeUsers; i++) {
-        
         let temp = createFakeUser();
         fakeUsers = fakeUsers.concat(temp);
     }
-    // console.log("fakeUSERS", fakeUsers);
-
-    let str = "";
-    let params = 22;
-    let j = 1;
-    for (let k = 0; k < desiredFakeUsers; k++) {
-        str = str.concat("(");
-        for (let i = 1; i < params; i++) {
-            str = i === params - 1 ? str.concat(`$${j}`) : str.concat(`$${j},`);
-            j++
-        }
-        str = k === desiredFakeUsers - 1 ? str.concat(")") : str.concat("),");
-    }
-
-    console.log("..writing data to the database");
-
-    await db.query(
-        `INSERT INTO users (username, first_name, last_name, email, status, 
-            password, birth_date, gender, sex_preference, email_notification,
-            online, latitude, longitude, country, real_time_notification,
-            fame_rating, bio, created_at, last_seen, profile_pic_path, fame_14_days)
-            VALUES ${str}`,
-        [...fakeUsers]
-    );
-    console.log(desiredFakeUsers, "more users");
+    return fakeUsers;
 };
+
+// insert fake users to the database
+const insertFakeUsers = async () => {
+    const fakeUsers = generateFakeUsers(desiredFakeUsers);
+    const stmt = prepareStmt(desiredFakeUsers);
+    console.log('\x1b[32m' + 'Generated 500+ fake users data' + '\x1b[0m');
+    try {
+        await db.query(stmt, [...fakeUsers]);
+        console.log('\x1b[32m' + 'Inserted 500+ fake accounts to the database' + '\x1b[0m');
+    } catch (err){
+        console.log("ERR", err);
+        
+    }
+};
+
+
+
+// const insertFakeUsers = async () => {
+//     var faker = require("faker");
+//     console.log("got to insertfake");
+//     const createFakeUser = () => {
+//     return [
+//         faker.internet.userName(),
+//         faker.name.firstName(),
+//         faker.name.lastName(),
+//         faker.internet.email(),
+//         "2",
+//         faker.internet.password(),
+//         faker.date.between("1960-01-01", "2002-01-01"),
+//         faker.random.arrayElement(["man", "woman"]),
+//         faker.random.arrayElement(["man", "woman", "both"]),
+//         false,
+//         faker.random.number(1),
+//         faker.address.latitude(),
+//         faker.address.longitude(),
+//         faker.address.country(),
+//         false,
+//         faker.random.float({ min: 0, max: 5, precision: 0.1 }),
+//         faker.lorem.sentences(),
+//         faker.date.between("2018-01-01", Date()),
+//         faker.date.between("2018-01-01", Date()),
+//         faker.image.avatar(),
+//         faker.random.number(30)
+//     ]}
+
+//     let fakeUsers = [];
+//     const desiredFakeUsers = 2;
+//     for (let i = 0; i < desiredFakeUsers; i++) {
+        
+//         let temp = createFakeUser();
+//         fakeUsers = fakeUsers.concat(temp);
+//     }
+//     // console.log("fakeUSERS", fakeUsers);
+
+//     let str = "";
+//     let params = 22;
+//     let j = 1;
+//     for (let k = 0; k < desiredFakeUsers; k++) {
+//         str = str.concat("(");
+//         for (let i = 1; i < params; i++) {
+//             str = i === params - 1 ? str.concat(`$${j}`) : str.concat(`$${j},`);
+//             j++
+//         }
+//         str = k === desiredFakeUsers - 1 ? str.concat(")") : str.concat("),");
+//     }
+
+   
+//     await db.query(
+//         `INSERT INTO users (username, first_name, last_name, email, status, 
+//             password, birth_date, gender, sex_preference, email_notification,
+//             online, latitude, longitude, country, real_time_notification,
+//             fame_rating, bio, created_at, last_seen, profile_pic_path, fame_14_days)
+//             VALUES ${str}`,
+//         [...fakeUsers]
+//     );
+//     console.log(desiredFakeUsers, "more users");
+// };
 
 module.exports = {
     getTags,

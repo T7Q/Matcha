61e0e4db3d6a6dab45e09f2572c5c5c57d000de8
T7Q <tkuumola@gmail.com>
2020-10-14 14:09:41 +0300

    feat: :art: remove console.log

--- backend/config/fake_db.js
+++ backend/config/fake_db.js
@@ -87,7 +87,6 @@ const insertFakeUsers = async () => {
             );
         })
         .catch((err) => {
-            console.log("here we got the error");
             console.log("\x1b[31m" + err + "\x1b[0m");
         })
         .finally(() => {

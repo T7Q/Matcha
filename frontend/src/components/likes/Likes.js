e656836ccb89da215f770484678d5c09d13c5c19
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-10-22 11:23:23 +0300

    style: :lipstick: update style on visits and likes

--- frontend/src/components/likes/Likes.js
+++ frontend/src/components/likes/Likes.js
@@ -1,15 +1,13 @@
-import React from "react";
-import { AppBar, Tabs, Tab, Typography } from "@material-ui/core";
-import Match from "../common/matchGallery/GetMatches";
+import React from 'react';
+import { AppBar, Tabs, Tab, Typography, Box, Container } from '@material-ui/core';
+import Match from '../common/matchGallery/GetMatches';
 
 const Likes = ({ match, history }) => {
     const { page } = match.params;
 
-    const indexToTabName = ["likesyou", "connected", "temp"];
+    const indexToTabName = ['likesyou', 'connected', 'temp'];
 
-    const [selectedTab, setValue] = React.useState(
-        indexToTabName.indexOf(page)
-    );
+    const [selectedTab, setValue] = React.useState(indexToTabName.indexOf(page));
 
     const handleChange = (event, newValue) => {
         history.push(`/likes/${indexToTabName[newValue]}`);
@@ -18,19 +16,27 @@ const Likes = ({ match, history }) => {
 
     return (
         <div>
-            <AppBar position="static">
-                <Typography variant="h6">Likes</Typography>
+            <AppBar color="secondary" position="static">
+                <Box p={2} justifyContent="center">
+                    <Typography variant="h6">Likes</Typography>
+                </Box>
                 <Tabs value={selectedTab} onChange={handleChange}>
                     <Tab label="Likes you" />
                     <Tab label="Connected" />
                 </Tabs>
             </AppBar>
-            {selectedTab === 0 && (
-                <Match route="/match/likedme" filterIsOn={0} />
-            )}
-            {selectedTab === 1 && (
-                <Match route="/match/connected" filterIsOn={0} />
-            )}
+            <Container>
+                {selectedTab === 0 && (
+                    <Box p={3}>
+                        <Match route="/match/likedme" filterIsOn={0} />
+                    </Box>
+                )}
+                {selectedTab === 1 && (
+                    <Box p={3}>
+                        <Match route="/match/connected" filterIsOn={0} />
+                    </Box>
+                )}
+            </Container>
         </div>
     );
 };

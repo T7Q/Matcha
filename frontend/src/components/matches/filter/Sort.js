f34f0beb6cc66f8b79351e1ecbfee0f14711711c
T7Q <tkuumola@gmail.com>
2020-10-26 10:14:21 +0200

    fix: :art: solving git conflict

--- frontend/src/components/matches/filter/Sort.js
+++ frontend/src/components/matches/filter/Sort.js
@@ -1,29 +1,14 @@
 import React from 'react';
 
-<<<<<<< HEAD
-import { TextField } from '@material-ui/core';
-import Autocomplete from '@material-ui/lab/Autocomplete';
-=======
 
 import { TextField } from "@material-ui/core";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import { filterStyles } from "../../../styles/filterStyles";
 
->>>>>>> 59d9de3f714b8f6b421664d51751e3cef5245e70
 
 const Sort = ({ updateFilter, setFilter, filterIsOn, filter }) => {
     const classesFilter = filterStyles();
     const sort = [
-<<<<<<< HEAD
-        { label: 'Yongest', db: 'age_asc' },
-        { label: 'Oldest', db: 'age_desc' },
-        { label: 'Best rating', db: 'fame_desc' },
-        { label: 'Lowest rating', db: 'fame_asc' },
-        { label: 'Closest', db: 'distance_asc' },
-        { label: 'Furtherst away', db: 'distance_desc' },
-        { label: 'Most interest in common', db: 'commonTag_desc' },
-        { label: 'Least interest in common', db: 'commonTag_asc' },
-=======
 
         { label: "Yongest First", db: "age_asc" },
         { label: "Oldest first", db: "age_desc" },
@@ -34,7 +19,6 @@ const Sort = ({ updateFilter, setFilter, filterIsOn, filter }) => {
         { label: "Most common interest", db: "commonTag_desc" },
         { label: "Least common interest", db: "commonTag_asc" },
 
->>>>>>> 59d9de3f714b8f6b421664d51751e3cef5245e70
     ];
 
     const handleSortChange = (event, newValue) => {
@@ -66,11 +50,6 @@ const Sort = ({ updateFilter, setFilter, filterIsOn, filter }) => {
             id="combo-sort"
             onChange={handleSortChange}
             options={sort}
-<<<<<<< HEAD
-            getOptionLabel={option => option.label}
-            getOptionSelected={option => option}
-            renderInput={params => <TextField {...params} value={findValue} label="Sort" />}
-=======
 
             getOptionLabel={(option) => option.label}
             getOptionSelected={(option) => option}
@@ -78,7 +57,6 @@ const Sort = ({ updateFilter, setFilter, filterIsOn, filter }) => {
             renderInput={(params) => <TextField {...params} value={findValue} label="Sort" 
             />}
 
->>>>>>> 59d9de3f714b8f6b421664d51751e3cef5245e70
         />
     );
 };

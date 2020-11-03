cb0ad89e85adde446175dbaf9285a28171f50950
T7Q <tkuumola@gmail.com>
2020-11-03 10:20:37 +0200

    feat: :art: update confirm, pwd change email text

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -13,16 +13,15 @@ const activateAccountEmail = (recipient, userId, username, token) => {
     const link = `${developmentUrl}/account/activate?user=${userId}&token=${token}`;
     const subject = 'Matcha: account activation';
     const content = `
-        <div style="font-size:16px;">
-            <h2 style="color:#514099; font-family:sans-serif;">
-                ${username.charAt(0).toUpperCase() + username.slice(1)}, welcome to Matcha!
-            </h2>
-            <p style="color:#735fc7;">
-                Here is your account activation <a href="${link}">link</a>
+        <div style="font-size:14px; background: #12172d; padding-top: 20px; padding-bottom: 20px;">
+             <h3 style="color: #219bf1; font-family:sans-serif; text-align: center;">
+                ${username.charAt(0).toUpperCase() + username.slice(1)}, welcome to Astro Matcha!
+             </h3>
+            <p style="color:white; text-align: center; padding-top: 5px;">
+                Here is your account activation <a style="color: #ca416e; font-weight: bold;" href="${link}">link</a>
             </p>
-            <p style="color:#735fc7;"><small>Matcha team</p>
+            <p style="color:#b5bad3; text-align: center; padding-top: 5px;"><small>Astro Matcha team</p>
         </div>`;
-
     sendMail(recipient, subject, content);
     return { msg: 'Activation link has been send to your email' };
 };
@@ -31,18 +30,20 @@ const pwdResetEmail = (recipient, userId, username, token, req) => {
     const link = `${developmentUrl}/updatePwd?user=${userId}&token=${token}`;
     const subject = 'Matcha: reset password';
     const content = `
-        <div style="font-size:16px;">
-            <h2 style="color:#514099; font-family:sans-serif;">
-                ${username.charAt(0).toUpperCase() + username.slice(1)}, welcome to Matcha!
-            </h2>
-            <p style="color:#735fc7;">
-                We received a password reset request. The link to reset your password is below.
-                If you did not make this request, you can ignore this email.
+        <div style="font-size:14px; background: #12172d; padding-top: 20px; padding-bottom: 20px;">
+            <h3 style="color: #219bf1; font-family:sans-serif; text-align: center;">
+                Hi, ${username.charAt(0).toUpperCase() + username.slice(1)}! You have submitted a password change request
+            </h3>
+            <p style="color:white; text-align: center; padding-top: 5px;">
+                If it was you, to change your Astro Match password click the link below.
             </p>
-            <p style="color:#735fc7;">
-                Here is your password reset <a href="${link}">link</a>
+            <p style="color:white; text-align: center; padding-top: 5px;">
+                <a style="color: #ca416e; font-weight: bold;" href="${link}">Reset my password</a>
             </p>
-            <p style="color:#735fc7;"><small>Matcha team</p>
+            <p style="color:#b5bad3; text-align: center; padding-top: 5px;"><small>Thank you for using Astro Matcha
+            </p>
+            <p style="color:#b5bad3; text-align: center; ">The Astro Matcha team
+            </p>
         </div>`;
 
     sendMail(recipient, subject, content);
